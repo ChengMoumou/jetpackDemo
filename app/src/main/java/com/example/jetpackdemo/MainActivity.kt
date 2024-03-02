b454407b7727fadd9b4d2232e2d9204b7241e3cf
Cara <956144678@qq.com>
2024-03-02 17:58:42 +0800

    初始化项目

--- app/src/main/java/com/example/jetpackdemo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackdemo/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.jetpackdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackdemo.ui.theme.JetpackDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +27,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("aaa", "bbb")){
+    Column(modifier) {
+        for (name in names){
+            Greeting(name = name)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded =  remember { mutableStateOf(false) }
+    val extend by animateDpAsState(if (expanded.value) 40.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ))
+
+
+    Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier.padding(3.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = modifier
+                .weight(1f)
+                .padding(bottom = extend.coerceAtLeast(0.dp))){
+                Text(text = "Hello !",)
+                Text(text = "$name")
+            }
+            ElevatedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(if(expanded.value) "show more" else "show less")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     JetpackDemoTheme {
-        Greeting("Android")
+       MyApp()
     }
 }
